2fd302f3a1f25961d0dfefc230dad6488a3dd34c
JeffMuraro7 <jeff.mrr@eduge.ch>
2015-11-03 08:28:16 +0100

    Mise en ligne du projets
    Première mise en ligne du projets.

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<!--
+    Crée le     : 1 nov. 2015, 08:56:15
+    Auteur      : Jeff Muraro
+    Version     : v0.1
+    Description : Music'Land est un projet qui se trouve dans le cadre du module m152. Nous sommes deux à travailler dessus.
+                  Ainsi M. Bertrand et M. Muraro vont réaliser un site web qui regroupe des musiques pour en faire un site
+                  de streaming tel que soundcloud.
+-->
+
+<?php
+        // put your code here
+?>
+<html>
+    <head>
+        <!-- Début des feuilles de styles -->
+        <link href="StyleCSS/myStyle.css" rel="stylesheet" type="text/css"/>
+        <link href="StyleCSS/myFont.css" rel="stylesheet" type="text/css"/>
+        <!-- Fin des feuilles de styles -->
+        <meta charset="UTF-8">
+        <title>Music'Land | Accueil</title>
+    </head>
+    <body>
+        
+        <!-- Bloc pour l'en-tête -->
+        <header>
+            En tête
+        </header>
+        
+        <!-- Bloc pour la navigation -->
+        <nav>
+            Navigation
+        </nav>
+        
+        <!-- Bloc pour le contenu du site -->
+        <section>
+            Section de contenue
+        </section>
+        
+        <!-- Bloc pour le pied de page -->
+        <footer>
+            Pied de page
+        </footer>
+        
+    </body>
+</html>
