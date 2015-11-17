7d92dcdd44c36649b40ccfb9a761de442d6a2616
Orion24 <nicolas.bertrand74@gmail.com>
2015-11-17 10:08:59 +0100

    Fichier de connexion à la base

---
+++ fonctionBD/connection_bd.php
@@ -0,0 +1,17 @@
+<?php
+  DEFINE('DB_HOST', "127.0.0.1");
+  DEFINE('DB_NAME', "music_land");
+  DEFINE('DB_USER', "musicLand");
+  DEFINE('DB_PASS', "SuperLand");
+  function getDb() {
+      static $dbb = null;
+      if ($dbb === null) {
+          try {
+              $dbb = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . '', DB_USER, DB_PASS);
+              $dbb->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+          } catch (PDOException $e) {
+              die('Erreur : ' . $e->getMessage());
+          }
+      }
+      return $dbb;
+    }
