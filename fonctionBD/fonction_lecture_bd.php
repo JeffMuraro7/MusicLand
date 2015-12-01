880af452140847185c73824e189f3ba54109947a
Orion24 <nicolas.bertrand74@gmail.com>
2015-12-01 11:07:53 +0100

    Upload de la musique

--- fonctionBD/fonction_lecture_bd.php
+++ fonctionBD/fonction_lecture_bd.php
@@ -2,8 +2,8 @@
   require_once 'connexion_bd.php';
   function login($username, $pass) {
     $request = getDb()->prepare("SELECT pseudo, MDP, Statut FROM user WHERE pseudo = :pseudo AND MDP = SHA1(:pass)");
-    $request->bindParam(':pseudo', $username);
-    $request->bindParam(':pass', $pass);
+    $request->bindParam(':pseudo', $username, PDO::PARAM_STR);
+    $request->bindParam(':pass', $pass, PDO::PARAM_STR);
     $request->execute();
     return $request->fetch(PDO::FETCH_ASSOC);
   }
@@ -11,7 +11,15 @@
   function recuperer_nom_album_par_artiste($nomArtiste)
   {
     $request = getDb()->prepare("SELECT NomAlbum, IdAlbum FROM album WHERE NomArtiste = :nom");
-    $request->bindParam(':nom', $nomArtiste);
+    $request->bindParam(':nom', $nomArtiste, PDO::PARAM_STR);
     $request->execute();
     return $request->fetchAll(PDO::FETCH_ASSOC);
+  }
+
+  function recuperer_nom_album_par_id($idAlbum)
+  {
+    $request = getDb()->prepare("SELECT NomAlbum FROM album WHERE IdAlbum = :id");
+    $request->bindParam(':id', $idAlbum, PDO::PARAM_INT);
+    $request->execute();
+    return $request->fetch(PDO::FETCH_ASSOC);
   }
